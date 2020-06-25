307d1f751d95223dd9db6ecbdbff07ef61af45f2
David Y. Stephenson <david@davidystephenson.com>
2020-06-25 12:29:35 +0200

    Give title to LOTR movie

---
+++ index.js
@@ -0,0 +1,7 @@
+const toTitleCase = require('to-title-case')
+
+console.log('toTitleCase test:', toTitleCase)
+
+const titled = toTitleCase('the lord of the rings: the fellowship of the ring')
+
+console.log('titled test:', titled)
